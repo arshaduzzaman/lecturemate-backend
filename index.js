7d71d9100eef965974843e1427fc05da557546a0
Arshad Uzzaman <arshaduzzaman156@live.com>
2025-04-21 06:45:40 +0600

    updated cors

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ const app = express();
 // Configure CORS to allow specific origins
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://lecture-mate.netlify.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://lecture-mate.netlify.app",
+      "https://lecturemate2.netlify.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
